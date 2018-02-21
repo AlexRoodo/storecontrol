0eccc259b673de90cb9c649fb4c52ba1c3ef39c6
aturchenkov <aturchenkov@tsconsulting.ru>
2018-02-21 13:02:51 +0300

    Minor changes

--- src/main/java/ru/tsconsulting/storecontrol/StartStoreSales.java
+++ src/main/java/ru/tsconsulting/storecontrol/StartStoreSales.java
@@ -6,8 +6,8 @@ public class StartStoreSales {
     public static void main(String[] args) {
         try {
             if (args.length != 1 || Integer.parseInt(args[0]) <= 0) {
-                System.out.println("Необходимо передать программе 1 аргумент - количество " +
-                        "покупателей.");
+                System.out.println("Необходимо передать программе 1 аргумент - " +
+                        "количество покупателей.");
                 throw new NumberFormatException();
             }
             Store store = new Store(Integer.parseInt(args[0]));
