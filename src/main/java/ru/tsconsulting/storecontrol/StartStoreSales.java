0898da9cb3d5dc6ecea5045c5f13bcf47fd50342
aturchenkov <aturchenkov@tsconsulting.ru>
2018-02-21 10:51:17 +0300

    Minor changes

--- src/main/java/ru/tsconsulting/storecontrol/StartStoreSales.java
+++ src/main/java/ru/tsconsulting/storecontrol/StartStoreSales.java
@@ -6,13 +6,14 @@ public class StartStoreSales {
     public static void main(String[] args) {
         try {
             if (args.length != 1 || Integer.parseInt(args[0]) <= 0) {
-                System.out.println("Необходимо передать программе 1 аргумент - количество покупателей");
+                System.out.println("Необходимо передать программе 1 аргумент - количество " +
+                        "покупателей.");
                 throw new NumberFormatException();
             }
             Store store = new Store(Integer.parseInt(args[0]));
             store.startSales();
         } catch (NumberFormatException e) {
-            System.out.println("Введен некорректный аргумент");
+            System.out.println("Введен некорректный аргумент.");
         }
     }
 }
